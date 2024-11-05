36afa6ecd9a42113c086db5747a22e2e30d4f375
vinyeee <yeevin@naver.com>
2024-11-05 13:19:41 +0900

    feat: 모듈 간 의존성 설정(6) #6

--- module-api/src/main/java/biz/bigtablet/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/biz/bigtablet/moduleapi/ModuleApiApplication.java
@@ -2,10 +2,18 @@ package biz.bigtablet.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+// Spring Boot 애플리케이션 시작 지점으로 설정하며, 지정된 패키지를 기준으로 컴포넌트를 검색
 @SpringBootApplication(
 		scanBasePackages = {"biz.bigtablet.moduleapi", "biz.bigtablet.modulecommon"}
 )
+// JPA에서 엔티티(Entity)를 검색할 때 지정된 패키지(biz.bigtablet.modulecommon.domain)를 기준으로 검색
+@EntityScan("biz.bigtablet.modulecommon.domain")
+// JPA 리포지토리(Repository) 인터페이스를 지정된 패키지(biz.bigtablet.modulecommon.repository)에서 찾아 사용하도록 설정
+@EnableJpaRepositories(basePackages = "biz.bigtablet.modulecommon.repository")
+
 public class ModuleApiApplication {
 
 	public static void main(String[] args) {
