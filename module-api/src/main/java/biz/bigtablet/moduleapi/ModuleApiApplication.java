083cac86696917ed01f020d790796d4516a21871
vinyeee <yeevin@naver.com>
2024-11-06 12:00:56 +0900

    feat: 모듈 간 의존성 설정(8) #8

--- module-api/src/main/java/biz/bigtablet/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/biz/bigtablet/moduleapi/ModuleApiApplication.java
@@ -1,11 +1,16 @@
 package biz.bigtablet.moduleapi;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
 		scanBasePackages = {"biz.bigtablet.moduleapi", "biz.bigtablet.modulecommon"}
 )
+@EntityScan("biz.bigtablet.modulecommon.domain")
+@EnableJpaRepositories(basePackages = "biz.bigtablet.modulecommon.repository")
 public class ModuleApiApplication {
 
 	public static void main(String[] args) {
